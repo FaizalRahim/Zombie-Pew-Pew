68e2e143ddb5fc914e259c01ee7aafba8eba216d
faizalrahim <faizzzmart@gmail.com>
2023-02-20 15:00:21 +0800

    Update 1.8
    Levels and new mobs added in

--- ZombieSurvivor/app.js
+++ ZombieSurvivor/app.js
@@ -1,39 +1,45 @@
 // Global 
 const start = document.getElementById("start")
 const mob = document.getElementById("mob");
-const human = document.getElementById("human");
+const mob2 = document.getElementById("mob2");
+const mob3 = document.getElementById("mob3");
+const mob4 = document.getElementById("mob4");
+const cat = document.getElementById("cat");
+const owl = document.getElementById("owl");
+const snake = document.getElementById("snake");
+const sheep = document.getElementById("sheep");
+const playerScore= document.getElementById("playerScore");
+const bullet = document.getElementById("bullets");
+const lv = document.getElementById("lv");
 
 // Game variables
-const playerScore= document.getElementById("playerScore");
-// const hp = document.getElementById("hp");
-// let playerhp = 15;
-// const runaway = () => {
-//     human.style.visibility= "hidden";
 
-// };
-// const attack = () => {
-//     mob.style.visibility= "hidden";
-//     playerhp --;
-//     hp.innerHTML= "HP : " + playerhp;
-// };
-const player = document.getElementById("player");
 let score = 0;
-const bullet = document.getElementById("bullets")
 let bullets = 15;
+let level = 1;
+let goal = 10;
+let mobSpawn = 1600;
+let mob2Spawn = 1000;
+let mob3Spawn = 750;
+let mob4Spawn = 500;
+let catSpawn = 1500;
+let owlSpawn = 900;
+let snakeSpawn = 650;
+let sheepSpawn = 400;
+
 
 
 // Intro
 
 start.addEventListener("click",()=>{
-    alert("Click on the zombies to kill them, avoid clicking on the baby! For each zombie killed, you will get 1 point and for each baby you click, you lose 1 point. Get 10 points to win.")
-    start.innerHTML ="Restart Game";
-    mob.style.visibility="visible";
-    human.style.visibility="visible";
-    scoreBox.style.visibility="visible";
+    alert("Click on the zombies to kill them, avoid clicking on the wildlife! For each zombie killed, you will get points and for each wildlife you click, you lose points. Get 10 points to proceed to next level.")
     score = 0;
-    bullets = 15;
-    bullets ++;
-
+    bullets = 16;
+    level =1;
+    start.innerHTML ="Restart Game";
+    lv.innerHTML = "Level: "+ level;
+    playerScore.innerHTML="Score : " + score;
+    scoreBox.style.visibility="visible";
 
 });
 
@@ -46,7 +52,80 @@ const randomNumber = (num) => {
 
 // console.log(randomNumber);
 
+// Generate npcs
+const newZombie = () => {
+    mob.style.visibility= "visible";
+    mob.style.top=randomNumber()+"px" ;
+    mob.style.left=randomNumber()+"px";
 
+};
+
+const newZombie2 = () => {
+    if(level >=5){
+    mob2.style.visibility= "visible";
+    mob2.style.top=randomNumber()+"px" ;
+    mob2.style.left=randomNumber()+"px";
+    }
+
+};
+
+const newZombie3 = () => {
+    if(level>=10){
+    mob3.style.visibility= "visible";
+    mob3.style.top=randomNumber()+"px" ;
+    mob3.style.left=randomNumber()+"px";
+    }
+};
+
+const newZombie4 = () => {
+    if(level>=13){
+    mob4.style.visibility= "visible";
+    mob4.style.top=randomNumber()+"px" ;
+    mob4.style.left=randomNumber()+"px";
+    }
+};
+
+const newCat = () => {
+    cat.style.visibility= "visible";
+    cat.style.top=randomNumber()+"px" ;
+    cat.style.left=randomNumber()+"px";
+
+};
+
+const newOwl = () => {
+    if(level >=5){
+    cat.style.visibility= "visible";
+    cat.style.top=randomNumber()+"px" ;
+    cat.style.left=randomNumber()+"px";
+    }
+};
+
+const newSnake = () => {
+    if(level >=10){
+    cat.style.visibility= "visible";
+    cat.style.top=randomNumber()+"px" ;
+    cat.style.left=randomNumber()+"px";
+    }
+};
+
+const newSheep = () => {
+    if(level >=13){
+    cat.style.visibility= "visible";
+    cat.style.top=randomNumber()+"px" ;
+    cat.style.left=randomNumber()+"px";
+    }
+};
+
+
+// Spawn rate
+setInterval(newZombie,mobSpawn);
+setInterval(newZombie2,mob2Spawn);
+setInterval(newZombie3,mob3Spawn);
+setInterval(newZombie4,mob4Spawn);
+setInterval(newCat,catSpawn);
+setInterval(newOwl,owlSpawn);
+setInterval(newSnake,snakeSpawn);
+setInterval(newSheep,sheepSpawn);
 
 // Kill Zombie
 
@@ -55,88 +134,88 @@ mob.addEventListener("click",()=>{
     playerScore.innerHTML= "Score : " + score;
 });
 
-const newZombie = () => {
-    mob.style.visibility= "visible";
-    const currentWidth = window.innerWidth;
-    const currentHeight = window.innerHeight;
-    mob.style.top=randomNumber()+"px" ;
-    mob.style.left=randomNumber()+"px";
-    // document.body.appendChild(mob);
-    // const zombieAttack = setTimeout(attack,3000);
-};
+mob2.addEventListener("click",()=>{ 
+    score +=2;
+    playerScore.innerHTML= "Score : " + score;
+});
 
-// A wild human appears
+mob3.addEventListener("click",()=>{ 
+    score +=3;
+    playerScore.innerHTML= "Score : " + score;
+});
 
-human.addEventListener("click",()=>{ 
+mob4.addEventListener("click",()=>{ 
+    score +=5;
+    playerScore.innerHTML= "Score : " + score;
+});
+
+// Wild Life appears
+
+cat.addEventListener("click",()=>{ 
     score --;
     playerScore.innerHTML="Score : " + score;
 })
 
+owl.addEventListener("click",()=>{ 
+    score -=2;
+    playerScore.innerHTML="Score : " + score;
+})
 
-const newHuman = () => {
-    human.style.visibility= "visible";
-    const currentWidth = window.innerWidth;
-    const currentHeight = window.innerHeight;
-    human.style.top=randomNumber()+"px" ;
-    human.style.left=randomNumber()+"px";
-    // document.body.appendChild(human);
-    // const humanRun = setTimeout(runaway,3000);
+snake.addEventListener("click",()=>{ 
+    score -=3;
+    playerScore.innerHTML="Score : " + score;
+})
 
-};
-
-setInterval(newZombie,5000);
-setInterval(newHuman,1500);
+sheep.addEventListener("click",()=>{ 
+    score -=5;
+    playerScore.innerHTML="Score : " + score;
+})
 
 // Dont randomly click
 
 document.addEventListener("click",()=>{ 
 
     bullets --;
-    bullet.innerHTML= "Bullets : " + bullets;
-    if(bullets>0 && score>= 10) {
-        let name= prompt("Please tell us your name");  
-        alert(name + " survived !!!");
-        // Level2();
-    }else if (bullets<1){
-        let name= prompt("Please tell us your name");  
-        alert(name + " became lunch !!!");
+    bullet.innerHTML= "Bullets: " + bullets;
+    if(bullets>=0 && score>= goal) {
+        levelUp();
 
-
+    }else if (bullets<0){
+        let name= prompt("Please tell us your name");  
+        alert(name + " became lunch at level "+ level+ " !!!");
     }
 });
 
+levelUp =()=> {
+    level++;
+    goal +=10;
+    bullets +=15;
+    lv.innerHTML = "Level: "+ level;
+    mobSpawn -= 100;
+    catSpawn -=100;
+    switch(level){
+        case(level>=13):
+        mob2Spawn -= 100;
+        mob3Spawn -= 100;
+        mob4Spawn -= 100;
+        owlSpawn -=100;
+        snakeSpawn -=100;
+        sheepSpawn -= 100;
+        break;
+        case(level>=10):
+        mob2Spawn -= 100;
+        mob3Spawn -= 100;
+        owlSpawn -=100;
+        snakeSpawn -=100;
+        break;
+        case(level>=5):
+        mob2Spawn -= 100;
+        owlSpawn -=100;
+        break;
+    };
+    if(catSpawn<=0){
+        let name= prompt("Please tell us your name");
+        alert(name + " survived !!!");
+    };
 
-
-// Level 2
-
-// const Level2 = () => {
-// document.body.removeChild(mob);
-// document.body.removeChild(human);
-// player.style.visibility="visible";
-// };
-
-
-// console.log(player);
-
-// //Move player
-
-// move =() => {
-// document.addEventListener("keydown",(event)=>{
-//     switch (event.key){
-//         case("ArrowDown"):
-//         player.style.top += 100+ "px"
-//         break;
-//         case("ArrowUp"):
-//         player.style.top -= 100 + "px"
-//         break;
-//         case("ArrowLeft"):
-//         player.style.left -= 100 + "px"
-//         break;
-//         case("ArrowRight"):
-//         player.style.left += 100 + "px"
-//         break;            
-//     } 
-// });
-// };
-
-// document.addEventListener("keydown",move());
+}
